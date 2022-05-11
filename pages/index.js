9384d6604087fc9342091f0a7d626ecf00df9b44
adarshkushwaha <adarshkushwaha296@gmail.com>
2022-05-11 21:36:51 +0530

    added blog details

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Bio from "../components/home/Bio";
-// import BlogCard from "../components/Blog/BlogCard";
+import BlogCard from "../components/Blog/BlogCard";
 // import FeaturedBlog from "../components/home/FeaturedBlog";
 import FeaturedProjects from "../components/home/FeaturedProjects";
 import { getFeaturedProjects, getFeaturedBlogs, getPosts } from "../services";
@@ -12,10 +12,10 @@ const HomePage = ({ projects, blogs }) => {
       <h2 className="font-bold text-xl my-8">Projects.</h2>
       <FeaturedProjects fProjects={projects} />
       <h2 className="font-bold text-xl my-8">Blogs.</h2>
-      {/*<div className="grid grid-cols-1 gap-6 mt-14 p-1">
+      <div className="grid grid-cols-1 gap-6 mt-14 p-1">
         {blogs?.map((blog, index) => <BlogCard key={index} blog={blog.node} />
         )}
-    </div>*/}
+      </div>
     </div >
   )
 }
